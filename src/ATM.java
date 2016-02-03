f07468c926eda006439948aacbf9d94b802d457e
PiratePowWow <Thi3v3ry>
2016-02-03 14:18:27 -0500

    1.3 Completed Using Classes

--- src/ATM.java
+++ src/ATM.java
@@ -9,32 +9,35 @@ public class ATM {
     public static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) throws Exception {
-        int balance = 100;
-        System.out.println("Hey, I'm ATM. What is your name?");
-        String name = scanner.nextLine();
-        System.out.println("Hello " + name + ". What would you like to do?");
-        System.out.println("1: Check Balance");
-        System.out.println("2: Withdraw Funds");
-        System.out.println("3: Cancel");
-        String selection = scanner.nextLine();
-        if (selection.equals("1")) {
-            System.out.println("You have chosen to check your balance. Your balance is $" + balance + ".");
-        }
-        else if (selection.equals("2")) {
-            System.out.println("You have chosen to withdraw funds. How much would you like to withdraw?");
-            int withdrawal = scanner.nextInt();
-            if (withdrawal <= balance) {
-                System.out.println("Please collect your $" + withdrawal + " below.");
-            } else {
-                throw new Exception("You do not have that much in your account. Have a nice day and please come again.");
-            }
-        }
-        else if (selection.equals("3")) {
-            System.out.println("You have chosen to cancel this session. Have a nice day and please come again.");
-        }
-        else {
-            throw new Exception("You have entered an invalid selection.");
-        }
+        Methods newCustomer = new Methods();
+        newCustomer.enterName();
+        newCustomer.enterSelection();
+//        int balance = 100;
+//        System.out.println("Hey, I'm ATM. What is your name?");
+//        String name = scanner.nextLine();
+//        System.out.println("Hello " + name + ". What would you like to do?");
+//        System.out.println("1: Check Balance");
+//        System.out.println("2: Withdraw Funds");
+//        System.out.println("3: Cancel");
+//        String selection = scanner.nextLine();
+//        if (selection.equals("1")) {
+//            System.out.println("You have chosen to check your balance. Your balance is $" + balance + ".");
+//        }
+//        else if (selection.equals("2")) {
+//            System.out.println("You have chosen to withdraw funds. How much would you like to withdraw?");
+//            int withdrawal = scanner.nextInt();
+//            if (withdrawal <= balance) {
+//                System.out.println("Please collect your $" + withdrawal + " below.");
+//            } else {
+//                throw new Exception("You do not have that much in your account. Have a nice day and please come again.");
+//            }
+//        }
+//        else if (selection.equals("3")) {
+//            System.out.println("You have chosen to cancel this session. Have a nice day and please come again.");
+//        }
+//        else {
+//            throw new Exception("You have entered an invalid selection.");
+//        }
 
     }
 }
--- src/Methods.java
+++ src/Methods.java
@@ -2,4 +2,53 @@
  * Created by PiratePowWow on 2/3/16.
  */
 public class Methods {
+    String name;
+    int balance = 100;
+    String selection;
+    int withdrawal;
+    public void enterName(){
+        System.out.println("Hey, I'm ATM. What is your name?");
+        name = ATM.scanner.nextLine();
+    }
+
+    public void enterSelection() throws Exception {
+        System.out.println("Hello " + name + ". What would you like to do?");
+        System.out.println("1: Check Balance");
+        System.out.println("2: Withdraw Funds");
+        System.out.println("3: Cancel");
+        selection = ATM.scanner.nextLine();
+        if (selection.equals("1")) {
+            checkBalance();
+        }
+        else if (selection.equals("2")){
+            withdraw();
+        }
+        else if (selection.equals("3")){
+            cancel();
+        }
+        else {
+            throw new Exception("You have entered an invalid selection.");
+        }
+    }
+
+    public void checkBalance(){
+        System.out.println("You have chosen to check your balance. Your balance is $" + balance + ".");
+    }
+
+    public void withdraw() throws Exception {
+        System.out.println("You have chosen to withdraw funds. How much would you like to withdraw?");
+        int withdrawal = ATM.scanner.nextInt();
+        if (withdrawal <= balance) {
+            System.out.println("Please collect your $" + withdrawal + " below.");
+            this.balance = balance - withdrawal;
+            System.out.println("Your new balance is $" + balance + ".");
+        } else {
+            throw new Exception("You do not have that much in your account. Have a nice day and please come again.");
+        }
+    }
+
+    public void cancel(){
+        System.out.println("You have chosen to cancel this session. Have a nice day and please come again.");
+    }
+
 }
